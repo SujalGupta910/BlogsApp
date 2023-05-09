4b9f83b3cca955e8feb51b10e058fe30986f1bca
Sujal Gupta <sujalgvs987@gmail.com>
2023-05-10 00:34:48 +0530

    Add project files.

---
+++ BlogApp/Models/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogApp.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public String BlogHeading { get; set; }
+        public String BlogContent { get; set; }
+        public Blog()
+        {
+
+        }
+    }
+}
